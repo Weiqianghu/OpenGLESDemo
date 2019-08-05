10acb44e57020f1813514d58837e3d98236ac473
weiqiang.hu <weiqiang.hu@liulishuo.com>
2019-08-05 20:02:17 +0800

    添加平移滤镜和缩放滤镜

--- app/src/main/java/com/liulishuo/speex/openglesdemo/FilterRenderer.kt
+++ app/src/main/java/com/liulishuo/speex/openglesdemo/FilterRenderer.kt
@@ -5,6 +5,7 @@ import android.opengl.GLES20
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 import kotlin.math.abs
+import kotlin.math.cos
 import kotlin.math.sin
 
 class FilterRenderer(context: Context) : BaseRenderer(context) {
@@ -19,6 +20,8 @@ class FilterRenderer(context: Context) : BaseRenderer(context) {
         filterList.add(GrayFilter(context))
         filterList.add(InverseFilter(context))
         filterList.add(LightUpFilter(context))
+        filterList.add(TranslateFilter(context))
+        filterList.add(ScaleFilter(context))
 
         currentFilter = filterList[0]
     }
@@ -116,6 +119,86 @@ class LightUpFilter(context: Context) : BaseFilter(context, VERTEX_SHADER, LIGHT
     override fun onDraw() {
         super.onDraw()
         val intensity = abs(sin((System.currentTimeMillis() - startTime) / 1000.0)) / 4.0
+        GLES20.glUniform1f(intensityLocation, intensity.toFloat())
+    }
+}
+
+class TranslateFilter(context: Context) : BaseFilter(context, VERTEX_SHADER, FRAGMENT_SHADER) {
+    companion object {
+        private const val FRAGMENT_SHADER = """
+            precision mediump float;
+            varying vec2 v_TexCoord;
+            uniform sampler2D u_TextureUnit;
+            uniform float xV;
+            uniform float yV;
+            
+            vec2 translate(vec2 srcCoord, float x, float y){
+                return vec2(srcCoord.x + x, srcCoord.y + y);
+            }
+            
+            void main(){
+                vec2 offsetTexCoord = translate(v_TexCoord, xV, yV);
+                if (offsetTexCoord.x >= 0.0 && offsetTexCoord.x <= 1.0 &&
+                        offsetTexCoord.y >= 0.0 && offsetTexCoord.y <= 1.0) {
+                        gl_FragColor = texture2D(u_TextureUnit, offsetTexCoord);
+                }
+            }
+        """
+    }
+
+    private var xLocation = 0
+    private var yLocation = 0
+    private var startTime: Long = 0
+
+    override fun onCreated() {
+        super.onCreated()
+        xLocation = GLES20.glGetUniformLocation(program, "xV")
+        yLocation = GLES20.glGetUniformLocation(program, "yV")
+        startTime = System.currentTimeMillis()
+    }
+
+    override fun onDraw() {
+        super.onDraw()
+        val intensity = sin((System.currentTimeMillis() - startTime) / 1000.0) * 0.5
+        GLES20.glUniform1f(xLocation, intensity.toFloat())
+        GLES20.glUniform1f(yLocation, 0.0f)
+    }
+}
+
+class ScaleFilter(context: Context) : BaseFilter(context, VERTEX_SHADER, FRAGMENT_SHADER) {
+    companion object {
+        private const val FRAGMENT_SHADER = """
+            precision mediump float;
+            varying vec2 v_TexCoord;
+            uniform sampler2D u_TextureUnit;
+            uniform float intensity;
+            
+            vec2 scale(vec2 srcCoord, float x, float y){
+                return vec2((srcCoord.x - 0.5)/x + 0.5, (srcCoord.y - 0.5)/y + 0.5);
+            }
+            
+            void main(){
+                vec2 offsetTexCoord = scale(v_TexCoord, intensity, intensity);
+                if (offsetTexCoord.x >= 0.0 && offsetTexCoord.x <= 1.0 &&
+                        offsetTexCoord.y >= 0.0 && offsetTexCoord.y <= 1.0) {
+                        gl_FragColor = texture2D(u_TextureUnit, offsetTexCoord);
+                }
+            }
+        """
+    }
+
+    private var intensityLocation = 0
+    private var startTime = 0L
+
+    override fun onCreated() {
+        super.onCreated()
+        intensityLocation = GLES20.glGetUniformLocation(program, "intensity")
+        startTime = System.currentTimeMillis()
+    }
+
+    override fun onDraw() {
+        super.onDraw()
+        val intensity = abs(cos((System.currentTimeMillis() - startTime) / 1000.0) * 1)
         GLES20.glUniform1f(intensityLocation, intensity.toFloat())
     }
 }
